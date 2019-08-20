a92ef722c624645c27456d3607d09f1e14a1ea28
Scott Penrose <scottp@dd.com.au>
2019-08-20 15:44:10 +1000

    Main quasar filess

---
+++ .quasar/import-quasar.js
@@ -0,0 +1,19 @@
+/**
+ * THIS FILE IS GENERATED AUTOMATICALLY.
+ * DO NOT EDIT.
+ *
+ * You are probably looking on adding initialization code.
+ * Use "quasar new plugin <name>" and add it there.
+ * One plugin per concern. Then reference the file(s) in quasar.conf.js > plugins:
+ * plugins: ['file', ...] // do not add ".js" extension to it.
+ **/
+
+import iconSet from 'quasar-framework/icons/fontawesome'
+
+
+import Vue from 'vue'
+
+import {Quasar,QLayout,QLayoutHeader,QLayoutFooter,QLayoutDrawer,QPageContainer,QPage,QToolbar,QToolbarTitle,QBtn,QIcon,QList,QListHeader,QItem,QItemMain,QItemSide,QInput,QCard,QCardTitle,QCardMain,QCardMedia,QCardSeparator,QCardActions,QSelect,QField,QChipsInput,QAutocomplete,QCheckbox,QPopover,QItemSeparator,QItemTile,QEditor,QTooltip,QSearch,QBtnDropdown,QParallax,QSpinner,QSpinnerAudio,QFab,QFabAction,QInnerLoading,QSpinnerGears,QBreadcrumbs,QBreadcrumbsEl,QDatetimePicker,QDatetime,QPagination,QTable,QTh,QTr,QTd,QTableColumns,QBtnGroup,QPageSticky,QModal,Ripple,CloseOverlay,BackToTop,Notify,Dialog,Loading} from 'quasar'
+
+
+Vue.use(Quasar, { config: {},iconSet: iconSet,components: {QLayout,QLayoutHeader,QLayoutFooter,QLayoutDrawer,QPageContainer,QPage,QToolbar,QToolbarTitle,QBtn,QIcon,QList,QListHeader,QItem,QItemMain,QItemSide,QInput,QCard,QCardTitle,QCardMain,QCardMedia,QCardSeparator,QCardActions,QSelect,QField,QChipsInput,QAutocomplete,QCheckbox,QPopover,QItemSeparator,QItemTile,QEditor,QTooltip,QSearch,QBtnDropdown,QParallax,QSpinner,QSpinnerAudio,QFab,QFabAction,QInnerLoading,QSpinnerGears,QBreadcrumbs,QBreadcrumbsEl,QDatetimePicker,QDatetime,QPagination,QTable,QTh,QTr,QTd,QTableColumns,QBtnGroup,QPageSticky,QModal},directives: {Ripple,CloseOverlay,BackToTop},plugins: {Notify,Dialog,Loading} })
