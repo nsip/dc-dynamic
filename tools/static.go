843311bdac557e677ec06087de7fe2f0cf58a1fa
Scott Penrose <scottp@dd.com.au>
2019-09-17 16:54:07 +1000

    Igore file

--- tools/static.go
+++ tools/static.go
@@ -1,7 +1,9 @@
 package main
 
 import "net/http"
+import "fmt"
 
 func main() {
+	fmt.Print("Static server started. http://localhost:8003/\n")
 	http.ListenAndServe(":8003", http.FileServer(http.Dir("./")))
 }
