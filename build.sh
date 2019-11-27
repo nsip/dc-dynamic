6bb7c0f30f9c01cde5b9e113e5e761bd20d7eefa
Scott Penrose <scottp@dd.com.au>
2019-11-28 04:38:28 +1100

    Release u p

--- build.sh
+++ build.sh
@@ -1,50 +1,19 @@
 #!/bin/bash
 
 set -e
+go get gopkg.in/cheggaaa/pb.v1
+cd tools; go build release.go; cd ..
 
-VERSION="v0.0.0"
+VERSION="$(./tools/release dc-dynamic version)"
+
+echo "BUILDING for $VERSION"
 
 CWD=`pwd`
-
-mkdir -p build
-mkdir -p build/Linux64/dc-dynamic
-mkdir -p build/Mac/dc-dynamic
-mkdir -p build/Win64/dc-dynamic
+npm install
 ./node_modules/.bin/quasar build
-rsync -av src/assets dist/spa-mat/
-rsync -av dist/spa-mat/* build/Linux64/dc-dynamic/
-rsync -av dist/spa-mat/* build/Mac/dc-dynamic/
-rsync -av dist/spa-mat/* build/Win64/dc-dynamic/
 
-echo "Building Linux64 binaries..."
-GOOS=linux
-GOARCH=amd64
-LDFLAGS="-s -w"
-OUTPUT=$CWD/build/Linux64/dc-dynamic
-GOOS="$GOOS" GOARCH="$GOARCH" go build -ldflags="$LDFLAGS" -o $OUTPUT/static tools/static.go
+cd dist/spa-mat/
+zip -qr ../../dc-dynamic-$VERSION.zip *
 
-echo "Building Win64 binaries..."
-GOOS=windows
-GOARCH=amd64
-LDFLAGS="-s -w"
-OUTPUT=$CWD/build/Win64/dc-dynamic
-GOOS="$GOOS" GOARCH="$GOARCH" go build -ldflags="$LDFLAGS" -o $OUTPUT/static.exe tools/static.go
-
-echo "Building Macintosh binaries..."
-GOOS=darwin
-GOARCH=amd64
-LDFLAGS="-s -w"
-OUTPUT=$CWD/build/Mac/dc-dynamic
-GOOS="$GOOS" GOARCH="$GOARCH" go build -ldflags="$LDFLAGS" -o $OUTPUT/static tools/static.go
-
-echo "ZIP Linux64"
-cd $CWD/build/Linux64
-zip -qr ../dc-dynamic-Linux64-$VERSION.zip *
-
-echo "ZIP Win64"
-cd $CWD/build/Win64
-zip -qr ../dc-dynamic-Win64-$VERSION.zip *
-
-echo "ZIP Mac"
-cd $CWD/build/Mac
-zip -qr ../dc-dynamic-Mac-$VERSION.zip *
+echo "If happy with zip, run the following release command"
+echo " ./tools/release dc-dynamic dc-dynamic.zip dc-dynamic-$VERSION.zip"
